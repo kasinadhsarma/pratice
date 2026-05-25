6414ed3ceb40fbc89d6e2d788fe7c1badad45aa3
kasinadhsarma <kasinadhsarma@gmail.com>
2026-05-25 18:08:36 +0530

    refactor: remove unused service directory and associated files

--- dsa/basics/numbers/leftangletriangle.py
+++
@@ -1,9 +0,0 @@
-n = 5
-
-for i in range(1, n + 1):
-    # Print (n - i) * 2 spaces to align the numbers on the right
-    print(" " * ((n - i) * 2), end="")
-    # Print the numbers from 1 to i, separated by spaces
-    for j in range(1, i + 1):
-        print(j, end=" ")
-    print()
--- dsa/basics/numbers/middletriangle.py
+++
@@ -1,9 +0,0 @@
-n = 5
-
-for i in range(1, n + 1):
-    # Print leading spaces to center the triangle
-    print(" " * (n - i), end="")
-    # Print numbers from 1 to i, separated by spaces
-    for j in range(1, i + 1):
-        print(j, end=" ")
-    print()
